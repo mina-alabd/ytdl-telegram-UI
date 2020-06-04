326328e16b588420d533f4b72f56054b74180bd6
mina-alabd <49130999+mina-alabd@users.noreply.github.com>
2020-06-04 21:09:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -120,6 +120,12 @@ bot.action(/^dl/, async (ctx) => {
 video.on('end', ()=>{
             ctx.reply(
 
+ctx.reply(`${title} \n
+الملف صالح لمده 48 ساعه \n
+${size} حجم الملف  \n
+${baseURL.href}${filename}`)
+            downloading.splice(downloading.indexOf(filename), 1)
+
 
 
 
