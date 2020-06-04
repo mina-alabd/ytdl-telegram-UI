06fe12693361f91fa0c6dbd1b258fc141fce574d
mina-alabd <49130999+mina-alabd@users.noreply.github.com>
2020-06-04 07:08:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ bot.action(/^dl/, async (ctx) => {
             //downloading.splice(downloading.indexOf(filename), 1)
 
 
-         ctx.reply(${filename})
+         ctx.reply(filename)
         sendVideoStatus(ctx)
 
         })
