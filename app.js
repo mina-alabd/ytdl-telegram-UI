537835f496707f0b3626c5eeec8805df8c5d9f9f
mina-alabd <49130999+mina-alabd@users.noreply.github.com>
2020-06-05 00:28:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ bot.action(/^dl/, async (ctx) => {
        
 video.on('end', ()=>{
 
-ctx.reply({baseURL.href + filename})
+ctx.reply(source: baseURL.href + filename)
          
               /*ctx.reply(`${title} \n
 الملف صالح لمده 48 ساعه \n
