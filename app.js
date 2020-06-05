56db1dad4a31aaa1389edea2bdc9480053617059
mina-alabd <49130999+mina-alabd@users.noreply.github.com>
2020-06-05 14:26:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -115,8 +115,7 @@ video.on('end', ()=>{
         
         
         
-    ctx.replyWithVideo(url: 'https://tagmon.herokuapp.com/c5a12ebe73425959e81e3c0f12682f21.mp4')
-
+    ctx.telegram.sendVideo({url: 'https://tagmon.herokuapp.com/c5a12ebe73425959e81e3c0f12682f21.mp4'})
         
 /*ctx.reply(`${title} \n
 الملف صالح لمده 48 ساعه \n
