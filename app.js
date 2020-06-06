b41ef5b3b8523b1f09ede5631408bfc9ac4a4249
mina-alabd <49130999+mina-alabd@users.noreply.github.com>
2020-06-06 05:43:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -112,7 +112,16 @@ bot.action(/^dl/, async (ctx) => {
         })
 
          
-            
+            bot.on('message', (ctx) => {
+  
+  // send file
+  ctx.replyWithVideo({ source: 'dl/'+filename })
+
+  // send stream
+  ctx.replyWithVideo({
+    source: fs.createReadStream('dl/'+filename)
+  })
+})
             
             
 video.on('end', ()=>{
@@ -135,32 +144,7 @@ downloading.splice(downloading.indexOf(filename), 1)
 
 
 
-bot.on('message', (ctx) => {
-  // resend existing file by file_id
-  ctx.replyWithSticker('123123jkbhj6b')
 
-  // send file
-  ctx.replyWithVideo({ source: '/path/to/video.mp4' })
-
-  // send stream
-  ctx.replyWithVideo({
-    source: fs.createReadStream('/path/to/video.mp4')
-  })
-
-  // send buffer
-  ctx.replyWithVoice({
-    source: Buffer.alloc()
-  })
-
-  // send url via Telegram server
-  ctx.replyWithPhoto('https://picsum.photos/200/300/')
-
-  // pipe url content
-  ctx.replyWithPhoto({
-    url: 'https://picsum.photos/200/300/?random',
-    filename: 'kitten.jpg'
-  })
-})
 
 
 
